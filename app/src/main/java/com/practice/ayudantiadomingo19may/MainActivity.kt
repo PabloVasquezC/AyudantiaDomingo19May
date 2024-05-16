66d72e8df4cd8a151d79f095b09d445d7a6f50af
Pablo V. from Nitro-5 <vascor.pablo@gmail.com>
2024-05-16 15:51:48 -0400

    text animation in MainActivity

--- app/src/main/java/com/practice/ayudantiadomingo19may/MainActivity.kt
+++ app/src/main/java/com/practice/ayudantiadomingo19may/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.practice.ayudantiadomingo19may
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,23 @@ class MainActivity : AppCompatActivity() {
 
         // Encuentra el botón por su ID
         val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.text)
+
+        // Crear un ObjectAnimator para rotar en el eje Z
+        val animator = ObjectAnimator.ofFloat(textView, "rotationY", 0f, 360f)
+        animator.duration = 8000  // Duración de la animación en milisegundos
+        animator.repeatCount = ObjectAnimator.INFINITE  // Repetir indefinidamente
+        animator.interpolator = LinearInterpolator()  // Interpolador lineal para rotación uniforme
+        animator.start()  // Iniciar la animación
+
+        // Asigna un click listener al botón
+        button.setOnClickListener {
+            // Crea una instancia de la clase SecondActivity
+            val intent = Intent(this, SecondActivity::class.java)
+            // Inicia la actividad
+            startActivity(intent)
+        }
+
 
 
     }
